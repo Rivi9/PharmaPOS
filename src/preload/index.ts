85fec1a64986f794520e9a3e1f85f38aa032c07d
Rivi9 <rivinduprabha@gmail.com>
2026-02-02 15:16:21 +0530

    feat: add inventory preload API bindings

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -43,7 +43,43 @@ const electronAPI = {
   checkStockAvailability: (productId: string) =>
     ipcRenderer.invoke(IPC_CHANNELS.STOCK_CHECK_AVAILABILITY, productId),
   deductStock: (productId: string, quantity: number) =>
-    ipcRenderer.invoke(IPC_CHANNELS.STOCK_DEDUCT, { productId, quantity })
+    ipcRenderer.invoke(IPC_CHANNELS.STOCK_DEDUCT, { productId, quantity }),
+
+  // Inventory - Products
+  listProducts: () => ipcRenderer.invoke(IPC_CHANNELS.PRODUCT_LIST),
+  createProduct: (data: unknown) => ipcRenderer.invoke(IPC_CHANNELS.PRODUCT_CREATE, data),
+  updateProduct: (id: string, data: unknown) =>
+    ipcRenderer.invoke(IPC_CHANNELS.PRODUCT_UPDATE, { id, data }),
+  deleteProduct: (id: string) => ipcRenderer.invoke(IPC_CHANNELS.PRODUCT_DELETE, id),
+  getLowStockProducts: () => ipcRenderer.invoke(IPC_CHANNELS.PRODUCT_LOW_STOCK),
+
+  // Inventory - Categories
+  listCategories: () => ipcRenderer.invoke(IPC_CHANNELS.CATEGORY_LIST),
+  createCategory: (data: unknown) => ipcRenderer.invoke(IPC_CHANNELS.CATEGORY_CREATE, data),
+  updateCategory: (id: string, data: unknown) =>
+    ipcRenderer.invoke(IPC_CHANNELS.CATEGORY_UPDATE, { id, data }),
+  deleteCategory: (id: string) => ipcRenderer.invoke(IPC_CHANNELS.CATEGORY_DELETE, id),
+
+  // Inventory - Suppliers
+  listSuppliers: () => ipcRenderer.invoke(IPC_CHANNELS.SUPPLIER_LIST),
+  createSupplier: (data: unknown) => ipcRenderer.invoke(IPC_CHANNELS.SUPPLIER_CREATE, data),
+  updateSupplier: (id: string, data: unknown) =>
+    ipcRenderer.invoke(IPC_CHANNELS.SUPPLIER_UPDATE, { id, data }),
+  deleteSupplier: (id: string) => ipcRenderer.invoke(IPC_CHANNELS.SUPPLIER_DELETE, id),
+
+  // Inventory - Stock Batches
+  listStockBatches: () => ipcRenderer.invoke(IPC_CHANNELS.STOCK_BATCH_LIST),
+  createStockBatch: (data: unknown) => ipcRenderer.invoke(IPC_CHANNELS.STOCK_BATCH_CREATE, data),
+  updateStockBatch: (id: string, data: unknown) =>
+    ipcRenderer.invoke(IPC_CHANNELS.STOCK_BATCH_UPDATE, { id, data }),
+  deleteStockBatch: (id: string) => ipcRenderer.invoke(IPC_CHANNELS.STOCK_BATCH_DELETE, id),
+  adjustStockBatch: (batchId: string, quantityChange: number, reason: string, userId: string) =>
+    ipcRenderer.invoke(IPC_CHANNELS.STOCK_BATCH_ADJUST, {
+      batchId,
+      quantityChange,
+      reason,
+      userId
+    })
 }
 
 contextBridge.exposeInMainWorld('electron', electronAPI)
